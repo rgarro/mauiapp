1ab0b5b92ee17c5908d43bf0750a554465f8ec4a
rgarro <rgarro@gmail.com>
2024-03-06 11:54:51 -0600

    calgary nagpas  sky gazing hookipa ..

--- MauiApp1/MauiProgram.cs
+++ MauiApp1/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace MauiApp1
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    Console.WriteLine("Quebec Bonbo sky gazing Hookipa skies!");
                 });
 
 #if DEBUG
